f445c37c9719bb9051bf2d152b745ba87a6b0db4
Shubham Thind <shubhamthind@192.168.1.3>
2025-10-12 13:14:48 +0530

    Initial Commit

---
+++ src/test/java/com/jobsportal/jobs_api/JobsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jobsportal.jobs_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
